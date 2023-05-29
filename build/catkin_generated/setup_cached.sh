6e2e37ecd763ab54f025a643be04915d9e6dbc15
eric-hyuk-ji <vx9200@hanyang.ac.kr>
2023-05-29 16:29:41 +0900

    first commit

---
+++ build/catkin_generated/setup_cached.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env sh
+# generated from catkin/python/catkin/environment_cache.py
+
+# based on a snapshot of the environment before and after calling the setup script
+# it emulates the modifications of the setup script without recurring computations
+
+# new environment variables
+
+# modified environment variables
+export CMAKE_PREFIX_PATH='/home/hyuk/catkin_ws/devel:/opt/ros/kinetic'
+export LD_LIBRARY_PATH='/home/hyuk/catkin_ws/devel/lib:/opt/ros/kinetic/lib:/opt/ros/kinetic/lib/x86_64-linux-gnu'
+export PATH='/opt/ros/kinetic/bin:/home/hyuk/bin:/home/hyuk/.local/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games:/snap/bin'
+export PKG_CONFIG_PATH='/home/hyuk/catkin_ws/devel/lib/pkgconfig:/opt/ros/kinetic/lib/pkgconfig:/opt/ros/kinetic/lib/x86_64-linux-gnu/pkgconfig'
+export PWD='/home/hyuk/catkin_ws/build'
+export ROS_PACKAGE_PATH='/home/hyuk/catkin_ws/src:/opt/ros/kinetic/share'
