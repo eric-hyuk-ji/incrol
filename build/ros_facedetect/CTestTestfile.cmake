ac2de318ff31762499192c2dca90c06294169aa6
eric-hyuk-ji <vx9200@hanyang.ac.kr>
2023-07-05 18:55:37 +0900

    git rm test

--- build/ros_facedetect/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/hyuk/catkin_ws/src/ros_facedetect
-# Build directory: /home/hyuk/catkin_ws/build/ros_facedetect
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
